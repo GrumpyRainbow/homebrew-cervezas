65a9ec686e39171b64d87d35798ab01e34d09327
Brian Bridges <brianb@phase2online.com>
2016-11-09 08:12:01 -0600

    Use sha256 instead of sha1

--- pdftk.rb
+++ pdftk.rb
@@ -13,7 +13,7 @@ class Pdftk < Formula
   homepage 'http://www.pdflabs.com/tools/pdftk-server'
   url 'https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg',
     :using => PkgExtract
-  sha1 '68361f759a658636a3d147504d90197c688bf209'
+  sha256 'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
 
   depends_on :macos => :el_capitan
 
